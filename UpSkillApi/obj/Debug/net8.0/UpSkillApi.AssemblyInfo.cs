5608e8bca4059ef3de768ccd6eadb44ebf62313b
osama <115135285+Osss1000@users.noreply.github.com>
2025-04-11 17:06:04 +0200

    test1

--- UpSkillApi/obj/Debug/net8.0/UpSkillApi.AssemblyInfo.cs
+++ UpSkillApi/obj/Debug/net8.0/UpSkillApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UpSkillApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad5edf88c307a58d88fe5631ff38afcf7d098943")]
 [assembly: System.Reflection.AssemblyProductAttribute("UpSkillApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UpSkillApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
