e1db20eb2a1ed039c1ae399159235eb38275c1a6
osama <115135285+Osss1000@users.noreply.github.com>
2025-05-01 21:53:51 +0300

    ,,,

---
+++ UpSkillApi/Migrations/20250501184809_lastupdatesfromantar.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UpSkillApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class lastupdatesfromantar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DocumentationPath",
+                table: "Organizations");
+
+            migrationBuilder.UpdateData(
+                table: "ApplicationStatuses",
+                keyColumn: "ApplicationStatusId",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 1, 18, 48, 9, 621, DateTimeKind.Utc).AddTicks(4550));
+
+            migrationBuilder.UpdateData(
+                table: "ApplicationStatuses",
+                keyColumn: "ApplicationStatusId",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 1, 18, 48, 9, 621, DateTimeKind.Utc).AddTicks(4560));
+
+            migrationBuilder.UpdateData(
+                table: "ApplicationStatuses",
+                keyColumn: "ApplicationStatusId",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 1, 18, 48, 9, 621, DateTimeKind.Utc).AddTicks(4560));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DocumentationPath",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "ApplicationStatuses",
+                keyColumn: "ApplicationStatusId",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 4, 30, 15, 16, 42, 287, DateTimeKind.Utc).AddTicks(5787));
+
+            migrationBuilder.UpdateData(
+                table: "ApplicationStatuses",
+                keyColumn: "ApplicationStatusId",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 4, 30, 15, 16, 42, 287, DateTimeKind.Utc).AddTicks(5791));
+
+            migrationBuilder.UpdateData(
+                table: "ApplicationStatuses",
+                keyColumn: "ApplicationStatusId",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 4, 30, 15, 16, 42, 287, DateTimeKind.Utc).AddTicks(5793));
+        }
+    }
+}
--- UpSkillApi/Models/Organization.cs
+++ UpSkillApi/Models/Organization.cs
@@ -7,15 +7,10 @@ namespace UpSkillApi.Models;
 public partial class Organization
 {
     public int OrganizationId { get; set; }
-
     public string? Description { get; set; }
-
-    public string? DocumentationPath { get; set; }
-    
     public int UserId { get; set; }
     
     public virtual User User { get; set; } = null!;
-
     public virtual ICollection<VolunteeringJob> VolunteeringJobs { get; set; } = new List<VolunteeringJob>();
     public string CommercialRecordPath { get; internal set; }
 }
